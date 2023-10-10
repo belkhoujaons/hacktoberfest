fab442e55030f52f51c1b8bbfd0f663e2bf3cdc1
ons.belkhouja <ons.belkhouja@proxym-it.com>
2023-10-10 12:40:52 +0100

    update my message

--- scripts/Hello_world_belkhoujaons.js
+++ scripts/Hello_world_belkhoujaons.js
@@ -4,4 +4,4 @@
 // GITHUB: https://github.com/belkhoujaons
 
 console.log('Hello, World!');
-console.log('\x1b[31m%s\x1b[0m', 'Hello all Tunisian people here !!!!')
+console.log('\x1b[31m%s\x1b[0m', 'Hello all Tunisian people here, love you !!!!')
